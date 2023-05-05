cc770c2fced4521486aacac6276ca381f08a9d67
叶加博 <jiabo.ye@stu.ecnu.edu.cn>
2023-05-05 21:00:20 +0800

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,13 +1,18 @@
-def get_model(checkpoint_path=None, tokenizer_path=None, peft_config=None):
-    import torch
-    from transformers.models.llama.configuration_llama import LlamaConfig
+import torch
+from peft import LoraConfig, TaskType, get_peft_config, get_peft_model
+from transformers.models.llama.configuration_llama import LlamaConfig
+from transformers.models.llama.tokenization_llama import LlamaTokenizer
 
-    from mplug_owl.configuration_mplug_owl import mPLUG_OwlConfig
-    from mplug_owl.modeling_mplug_owl import mPLUG_OwlForConditionalGeneration
-    from peft import get_peft_config, get_peft_model, LoraConfig, TaskType
+from mplug_owl.configuration_mplug_owl import mPLUG_OwlConfig
+from mplug_owl.modeling_mplug_owl import (ImageProcessor,
+                                          mPLUG_OwlForConditionalGeneration)
+from mplug_owl.tokenize_utils import tokenize_prompts
+
+
+def get_model(checkpoint_path=None, tokenizer_path=None, peft_config=None, device='cuda', dtype=torch.bfloat16):
 
     config = mPLUG_OwlConfig()
-    model = mPLUG_OwlForConditionalGeneration(config=config).to(torch.bfloat16)
+    model = mPLUG_OwlForConditionalGeneration(config=config)
     model.eval()
 
     if checkpoint_path is not None:
@@ -19,30 +24,28 @@ def get_model(checkpoint_path=None, tokenizer_path=None, peft_config=None):
         msg = model.load_state_dict(tmp_ckpt, strict=False)
         print(msg)
 
-    import torch
-    from transformers.models.llama.tokenization_llama import LlamaTokenizer
-
-    from mplug_owl.modeling_mplug_owl import ImageProcessor
     assert tokenizer_path is not None
     tokenizer = LlamaTokenizer(
         tokenizer_path, pad_token='<unk>', add_bos_token=False)
     tokenizer.eod_id = tokenizer.eos_token_id
     img_processor = ImageProcessor()
+
+    model = model.to(device)
+    model = model.to(dtype)
     return model, tokenizer, img_processor
 
 
-def do_generate(prompts, image_list, model, tokenizer, img_processor, **generate_kwargs):
-    import requests
-    import torch
-    from PIL import Image
-
-    from mplug_owl.tokenize_utils import tokenize_prompts
+def do_generate(prompts, image_list, model, tokenizer, img_processor, device='cuda', dtype=torch.bfloat16, **generate_kwargs):
+    
     tokens_to_generate = 0
     add_BOS = True
     context_tokens_tensor, context_length_tensorm, attention_mask = tokenize_prompts(
         prompts=prompts, tokens_to_generate=tokens_to_generate, add_BOS=add_BOS, tokenizer=tokenizer, ignore_dist=True)
-    images = img_processor(image_list).to(torch.bfloat16)
+    images = img_processor(image_list).to(dtype)
     model.eval()
+    images = images.to(device)
+    context_tokens_tensor = context_tokens_tensor.to(device)
+    attention_mask = attention_mask.to(device)
     with torch.no_grad():
         res = model.generate(input_ids=context_tokens_tensor, pixel_values=images,
                              attention_mask=attention_mask, **generate_kwargs)
@@ -53,9 +56,8 @@ def do_generate(prompts, image_list, model, tokenizer, img_processor, **generate
 if __name__ == '__main__':
     from interface import get_model
     model, tokenizer, img_processor = get_model(
-            checkpoint_path='checkpoint path', tokenizer_path='tokenizer path')
-    prompts = [
-'''The following is a conversation between a curious human and AI assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
+        checkpoint_path='checkpoint path', tokenizer_path='tokenizer path')
+    prompts = ['''The following is a conversation between a curious human and AI assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
 Human: <image>
 Human: Explain why this meme is funny.
 AI: ''']
